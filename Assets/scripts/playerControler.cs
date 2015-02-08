cde735a94b31fdb0ba5ea3cc70bc45ed07c9b3cb
Rob <rlw115@scarletmail.rutgers.edu>
2015-02-08 04:23:42 -0500

    RollABall
    This was the Unity Tutorial game: Roll a Ball. It is simple and crude.

---
+++ Assets/scripts/playerControler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControler : MonoBehaviour {
+
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movement*speed*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+			if(count >=3)
+				winText.text = "YOU WIN!";
+		}
+		//Destroy(other.gameObject);
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+	}
+}
